cf69147aaad7e82a773b7ed46a45de12bdd18f02
Mohammad <muhra.it@gmail.com>
2026-03-02 19:53:32 +0100

    Harden website SEO and update mail sender

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { hydrateRoot, createRoot } from "react-dom/client";
 import { LandingApp } from "./components/LandingApp";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+const app = (
   <React.StrictMode>
     <LandingApp />
   </React.StrictMode>
 );
+
+if (rootElement?.hasChildNodes()) {
+  hydrateRoot(rootElement, app);
+} else if (rootElement) {
+  createRoot(rootElement).render(app);
+}
